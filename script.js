fface0c8e27b003bba6831800bfad266d065773d
jamesdarrelbautista <jamesdarrel.bautista@g.msuiit.edu.ph>
2022-03-12 17:48:21 +0800

    Done with the step 3

---
+++ script.js
@@ -0,0 +1,12 @@
+let weapon = [`rock`, `paper`, `scissors`];
+
+let randomIndexNumber = Math.floor(Math.random() * weapon.length);
+
+let computerPlay = weapon => console.log(weapon[randomIndexNumber]);
+
+
+let singleRound = (playerSelection, computerSelection) => {
+    
+
+
+}
